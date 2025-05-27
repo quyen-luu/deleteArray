f6a48f9b01fbfc9bc1864c5593745e5f6098f73e
quyen-luu <luunguyenthaoquyen@gmail.com>
2025-05-27 21:20:49 +0700

    Commit
    Add content

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿public class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] arrays = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        Console.Write("Nhap phan tu can xoa: ");
+        int x = Convert.ToInt32(Console.ReadLine());
+        Funtionexample(x, arrays);
+    }
+    public static void Funtionexample(int x, int[] arrays)
+    {
+        int index = 0;
+        for (int i = 0; i < arrays.Length; i++)
+        {
+            if (arrays[i] == x)
+            {
+                index = i;
+            }
+        }
+        for (int j = index; j < arrays.Length - 1; j++)
+        {
+            arrays[j] = arrays[j + 1];
+        }
+        for (int i = 0; i < arrays.Length - 1; i++)
+        {
+            Console.Write(arrays[i] + "; ");
+        }
+    }
+}
